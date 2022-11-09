ba0e20d1de174d4bb507e9eae60dc13921fd205a
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-11-09 14:56:11 -0500

    added authentication

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,20 @@
 class CategoryController < Sinatra::Base
+    set :bind, '0.0.0.0'
+    configure do
+        enable :cross_origin
+    end
+    before do
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    end
+    
+    # routes...
+    options "*" do
+        response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        200
+    end
+    
     get '/categories' do
         categories = Category.all
         categories.to_json
